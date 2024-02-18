c295b3607cd0699494fa6e49d5f895d85db4e74d
Erkki Seppälä <flux@inside.org>
2024-02-18 19:44:20 +0200

    expiring: added getters; renamed values from value

--- backend/src/expiring.rs
+++ backend/src/expiring.rs
@@ -1,17 +1,19 @@
 use std::collections::{BTreeMap, HashMap};
 
+#[derive(Clone)]
 struct ExpiringData<Value> {
     value: Value,
     insert_count: u64,
     insert_time: chrono::DateTime<chrono::Utc>,
 }
 
+#[derive(Clone)]
 pub(crate) struct Expiring<Key, Value>
 where
     Key: Clone,
     Value: Clone,
 {
-    value: HashMap<Key, ExpiringData<Value>>,
+    values: HashMap<Key, ExpiringData<Value>>,
     by_insert: BTreeMap<u64, Key>,
     insert_count: u64,
     expire_duration: chrono::Duration,
@@ -24,16 +26,45 @@ where
 {
     pub fn new(expire_duration: chrono::Duration) -> Self {
         Expiring {
-            value: HashMap::new(),
+            values: HashMap::new(),
             by_insert: BTreeMap::new(),
             insert_count: 0u64,
             expire_duration,
         }
     }
 
+    pub fn get(&self, key: &Key) -> Option<&Value> {
+        self.values.get(key).map(|v| &v.value)
+    }
+
+    pub fn get_mut(&mut self, key: &Key) -> Option<&mut Value> {
+        self.values.get_mut(key).map(|v| &mut v.value)
+    }
+
+    // Returns true if a new value was inserted (with the put function)
+    pub fn get_or_put_mut<F>(&mut self, key: Key, put: F) -> (bool, &mut Value)
+    where
+        F: FnOnce() -> Value,
+    {
+        let entry = self.values.entry(key.clone());
+        let was_vacant = matches!(entry, std::collections::hash_map::Entry::Vacant(_));
+        let value = entry.or_insert_with(|| ExpiringData {
+            value: put(),
+            insert_count: self.insert_count,
+            insert_time: chrono::Utc::now(),
+        });
+
+        if was_vacant {
+            self.by_insert.insert(self.insert_count, key.clone());
+            self.insert_count += 1;
+        }
+
+        (was_vacant, &mut value.value)
+    }
+
     pub fn put(&mut self, key: Key, value: Value) {
         self.remove(&key.clone());
-        self.value.insert(
+        self.values.insert(
             key.clone(),
             ExpiringData {
                 value: value,
@@ -45,12 +76,26 @@ where
         self.insert_count += 1
     }
 
+    pub fn refresh(&mut self, key: &Key) -> bool {
+        match self.values.get_mut(key) {
+            None => false,
+            Some(ref mut v) => {
+                self.by_insert.remove(&v.insert_count);
+                v.insert_count = self.insert_count;
+                v.insert_time = chrono::Utc::now();
+                self.by_insert.insert(v.insert_count.clone(), key.clone());
+                self.insert_count += 1;
+                true
+            }
+        }
+    }
+
     pub fn remove(&mut self, key: &Key) {
-        match self.value.get(key) {
+        match self.values.get(key) {
             None => (),
             Some(value) => {
                 self.by_insert.remove(&value.insert_count);
-                self.value.remove(key);
+                self.values.remove(key);
             }
         }
     }
@@ -60,7 +105,7 @@ where
         loop {
             match self.by_insert.first_key_value() {
                 None => break,
-                Some((_, key)) if self.value.get(key).unwrap().insert_time < deadline => {
+                Some((_, key)) if self.values.get(key).unwrap().insert_time < deadline => {
                     self.remove(&key.clone());
                 }
                 Some(_) => break,
@@ -69,13 +114,17 @@ where
     }
 
     pub fn all(&self) -> HashMap<Key, Value> {
-        self.value
+        self.values
             .iter()
             .map(|(k, v)| (k.clone(), v.value.clone()))
             .collect()
     }
 
+    pub fn all_ref(&self) -> HashMap<&Key, &Value> {
+        self.values.iter().map(|(k, v)| (k, &v.value)).collect()
+    }
+
     pub fn is_empty(&self) -> bool {
-        self.value.is_empty()
+        self.values.is_empty()
     }
 }
